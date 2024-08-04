1a72858ac9348c4aeeb9613a83d851276ec33105
itzmexuan06 <23023432@myrp.edu.sg>
2024-08-04 17:23:52 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('App JavaScript file loaded on all pages.');
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
